0e79573e9c8d8c920d9350469cfcda6be55ced87
Mark <techliujing@gmail.com>
2024-03-01 01:20:29 +0800

    update files structure

---
+++ ios/AHelloWorld/Classes/NativeModule.h
@@ -0,0 +1,17 @@
+//
+//  NativeModule.h
+//  AHelloWorld
+//
+//  Created by Mark Lewis on 2024/3/1.
+//  Copyright © 2024 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NativeModule : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/AHelloWorld/Classes/NativeModule.m
@@ -0,0 +1,13 @@
+//
+//  NativeModule.m
+//  AHelloWorld
+//
+//  Created by Mark Lewis on 2024/3/1.
+//  Copyright © 2024 Facebook. All rights reserved.
+//
+
+#import "NativeModule.h"
+
+@implementation NativeModule
+
+@end
