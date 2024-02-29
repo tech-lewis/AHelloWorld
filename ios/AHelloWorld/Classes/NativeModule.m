e7bdebc114113acdb38a081b690460c2eebcedcf
Mark <techliujing@gmail.com>
2024-03-01 01:47:57 +0800

    文件更新

--- ios/AHelloWorld/Classes/NativeModule.m
+++ ios/AHelloWorld/Classes/NativeModule.m
@@ -7,7 +7,22 @@
 //
 
 #import "NativeModule.h"
-
+#import "RCTBridgeModule.h"
+#import "AppDelegate.h"
+#import "WebBrowserViewController.h"
 @implementation NativeModule
+
+RCT_EXPORT_METHOD(RNOpenWebView:(NSString *)msg)
+{
+  dispatch_async(dispatch_get_main_queue(), ^{
+    AppDelegate *delegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+//    [delegate.navController pushViewController: gameController
+//                                      animated:true];
+    WebBrowserViewController *webController = [WebBrowserViewController new];
+    [webController loadRequestWithUrlString:@"https://www.baidu.com/s?wd=当地时间"];
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:webController];
+    [delegate.window.rootViewController presentViewController:navController animated:true completion:NULL];
+  });
+}
 
 @end
