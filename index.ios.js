36e38bb2619eb426ce050512f99fe6b6262ba2e2
Mark <techliujing@gmail.com>
2024-03-01 01:07:20 +0800

    更新版本为0.0.1， 使用SVG框架

--- index.ios.js
+++ index.ios.js
@@ -35,30 +35,8 @@ class AHelloWorld extends Component {
   // 写结构和内容
   render() {
     return (
-      <View style={styles.container}>
-        <Main/>
-        <Text style={styles.title}>{this.state.binaryTitle}</Text>
-        <TextInput style={styles.input} onChangeText={binary => {
-          if (/^[01]*$/.test(binary)) {
-            this.setState({ binary })
-            this.convertBinaryToOther(binary)
-          }
-        }} />
-        <Text style={styles.title}>{this.state.octalTitle}</Text>
-        <Text style={styles.output}>{this.state.octal}</Text>
-        <Text style={styles.title}>{this.state.decimalTitle}</Text>
-        <Text style={styles.output}>{this.state.decimal}</Text>
-        <Text style={styles.title}>{this.state.hexTitle}</Text>
-        <Text style={styles.output}>{this.state.hex}</Text>
-        {/* 
-        <Button
-          title="Toggle Title"
-          onPress={() =>
-            this.setState({ showTitle: !this.state.showTitle })
-          }
-        /> */}
-      </View>
-    );
+      <Main style={styles.container} />
+    )
   }
 }
 
